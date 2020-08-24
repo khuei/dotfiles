53467de62fb0bbea5c60c85bfd256a10ce868ac7
Khue Nguyen <Z5483Y@gmail.com>
2020-08-24 10:48:34 -0400

    feat(zsh): use LS_COLORS to colorize completion entries

--- .zsh/config/completion.zsh
+++ .zsh/config/completion.zsh
@@ -10,6 +10,6 @@ zstyle ':completion:*' accept-exact '*(N)'
 zstyle ':completion:*' use-cache on
 zstyle ':completion:*' menu select
 zstyle ':completion:*' matcher-list '' '+m:{[:lower:]}={[:upper:]}' '+m:{[:upper:]}={[:lower:]}' '+m:{_-}={-_}' 'r:|[._-]=* r:|=*' 'l:|=* r:|=*'
-zstyle ':completion:*' list-colors ''
+zstyle ':completion:*' list-colors "$LS_COLORS"
 zstyle ':completion:*' group-name ''
 zstyle ':completion:*:descriptions' format %F{default}%B--- %d ---%b%f
