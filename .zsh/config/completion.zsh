7d0cca579b154e6b9e5fcebfdc381eccb3bfb6bf
Khue Nguyen <Z5483Y@gmail.com>
2020-08-31 02:09:47 -0400

    feat(zsh): new completion method
    - Try case-insensitve first
    - Then fall back to fuzzy matching

--- .zsh/config/completion.zsh
+++ .zsh/config/completion.zsh
@@ -9,7 +9,10 @@ compinit -i -u
 zstyle ':completion:*' accept-exact '*(N)'
 zstyle ':completion:*' use-cache on
 zstyle ':completion:*' menu select
-zstyle ':completion:*' matcher-list '' '+m:{[:lower:]}={[:upper:]}' '+m:{[:upper:]}={[:lower:]}' '+m:{_-}={-_}' 'r:|[._-]=* r:|=*' 'l:|=* r:|=*'
+zstyle ':completion:*' matcher-list \
+	'+m:{[:lower:]}={[:upper:]}' \
+	'+m:{[:upper:]}={[:lower:]}' \
+	'r:|?=** m:{a-z\-}={A-Z\_}'
 zstyle ':completion:*' list-colors "$LS_COLORS"
 zstyle ':completion:*' group-name ''
 zstyle ':completion:*:descriptions' format %F{default}%B--- %d ---%b%f
