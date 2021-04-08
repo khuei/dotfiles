e325628c5dc9e66bc13c630faec9e6fbd4d0f117
Khue Nguyen <Z5483Y@gmail.com>
2021-04-08 07:54:59 -0400

    refactor(nvim): prefer packadd over packadd!

--- nvim/init.lua
+++ nvim/init.lua
@@ -7,15 +7,15 @@ end
 
 if vim.api.nvim_get_option('loadplugins') then
 	if vim.fn.has('packages') == 1 then
-		vim.cmd([[packadd! deoplete]])
-		vim.cmd([[packadd! deoplete-lsp]])
-		vim.cmd([[packadd! fzy-native]])
-		vim.cmd([[packadd! lspconfig]])
-		vim.cmd([[packadd! plenary]])
-		vim.cmd([[packadd! popup]])
-		vim.cmd([[packadd! telescope]])
-		vim.cmd([[packadd! treesitter]])
-		vim.cmd([[packadd! ultisnips]])
+		vim.cmd([[packadd deoplete]])
+		vim.cmd([[packadd deoplete-lsp]])
+		vim.cmd([[packadd fzy-native]])
+		vim.cmd([[packadd lspconfig]])
+		vim.cmd([[packadd plenary]])
+		vim.cmd([[packadd popup]])
+		vim.cmd([[packadd telescope]])
+		vim.cmd([[packadd treesitter]])
+		vim.cmd([[packadd ultisnips]])
 	end
 end
 
