55db5c8c2a0d14a6e4c7817d3ba5924bd7631dbf
Khue Nguyen <Z5483Y@gmail.com>
2021-09-25 00:00:38 -0400

    refactor(nvim): replace compe with coq

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,13 +1,13 @@
 require('plugin.mappings')
 require('plugin.settings')
 
-if vim.fn.executable('python3') == 1 then
-	vim.api.nvim_set_var('python3_host_prog', 'python3')
-end
+vim.g.coq_settings = {
+	auto_start = 'shut-up',
+}
 
 if vim.api.nvim_get_option('loadplugins') then
 	if vim.fn.has('packages') == 1 then
-		vim.cmd([[packadd compe]])
+		vim.cmd([[packad coq]])
 		vim.cmd([[packadd lspconfig]])
 		vim.cmd([[packadd treesitter]])
 	end
